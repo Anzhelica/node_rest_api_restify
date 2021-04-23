4dea9afb31718e058d50ea29595b402baa96d9be
Anzhelica <anzhelica.protsenko@gmail.com>
2021-04-23 12:10:25 +0300

    add a project

---
+++ auth.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+exports.authenticate = (email, password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            //Get User by email
+            const user = await User.findOne({ email });
+
+            // MAtch Password
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if (err) throw err;
+                if (isMatch) {
+                    resolve(user);
+                } else {
+                    //Pass didn't match
+                    reject('Authentication Failed');
+
+                }
+            });
+
+        } catch (err) {
+            // Email not found
+            reject('Authentication Failed');
+        }
+    });
+
+    
+}
